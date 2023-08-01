1223ce295fa50830da735fa9f62e9c4265add813
Iamneo <techadmin@iamneo.ai>
2023-08-01 14:59:31 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Button from "../Button/Button";
-
+import style from "../style.css"
 
 export const Card = ({key , question , correctAnswerMarkUpdate , attempt , options , answer }) => {
     const [dis,setDis] = useState(false);
