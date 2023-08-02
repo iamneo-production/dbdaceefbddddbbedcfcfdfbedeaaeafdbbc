a31eb535c86d26b9de4a3a07871fa2e2631123f2
Iamneo <techadmin@iamneo.ai>
2023-08-02 13:30:48 +0000

    Backup Commit

--- reactapp/src/tests/Card.test.js
+++ reactapp/src/tests/Card.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../components/UI/Card/Card";
-//import { shallow, mount } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 var questionProp, wrapper, mockFunction;
 
