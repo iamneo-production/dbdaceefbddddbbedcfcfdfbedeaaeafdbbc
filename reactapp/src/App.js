ae3129461ad2fe58fe33b09ce2e9160d6fcfda1a
Iamneo <techadmin@iamneo.ai>
2023-08-02 13:18:53 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Banner from './components/UI/Banner/Banner';
 import Button from './components/UI/Button/Button';
 import Card from './components/UI/Card/Card';
-import questions from './Questions/questions';
+import questions from './Data/questions';
 
 class App extends React.Component{
 
@@ -54,3 +54,108 @@ class App extends React.Component{
       currQuestion: this.state.currQuestion+1
     })
   }
+  render(){
+    return (
+    <div>
+      <h1>Quizz App</h1>
+      {this.state.showResultBanner&&(
+        <Banner score={this.state.questionsCorrect} />
+      )}
+      {this.state.showStart&&(
+        <div>
+          <Button onClick={this.startHandler} disabled={false} >Start Quiz</Button>
+        </div>
+      )}
+      {!(this.state.showStart)&&(
+        <div>
+          <div className="parent">
+                <div className="child">
+                    <Card 
+                        key={questions[0].questionId} 
+                        question={questions[0].question}
+                        correctAnswerMarkUpdate={this.updateScore}
+                        attempt={this.nextQuest}
+                        options={{
+                            option1: questions[0].option1,
+                            option2: questions[0].option2,
+                            option3: questions[0].option3,
+                            option4: questions[0].option4
+                        }}
+                        answer={questions[0].answer}
+                    />
+                </div>
+                <div className="child">
+                    <Card 
+                        key={questions[1].questionId} 
+                        question={questions[1].question}
+                        correctAnswerMarkUpdate={this.updateScore}
+                        attempt={this.nextQuest}
+                        options={{
+                            option1: questions[1].option1,
+                            option2: questions[1].option2,
+                            option3: questions[1].option3,
+                            option4: questions[1].option4
+                        }}
+                        answer={questions[1].answer}
+                    />
+                </div>
+            </div>
+            <div>
+                <div className="child">
+                    <Card 
+                        key={questions[2].questionId} 
+                        question={questions[2].question}
+                        correctAnswerMarkUpdate={this.updateScore}
+                        attempt={this.nextQuest}
+                        options={{
+                            option1: questions[2].option1,
+                            option2: questions[2].option2,
+                            option3: questions[2].option3,
+                            option4: questions[2].option4
+                        }}
+                        answer={questions[2].answer}
+                    />
+                </div>
+                <div className="child">
+                    <Card 
+                        key={questions[3].questionId} 
+                        question={questions[3].question}
+                        correctAnswerMarkUpdate={this.updateScore}
+                        attempt={this.nextQuest}
+                        options={{
+                            option1: questions[3].option1,
+                            option2: questions[3].option2,
+                            option3: questions[3].option3,
+                            option4: questions[3].option4
+                        }}
+                        answer={questions[3].answer}
+                    />
+                </div>
+                <div className="child">
+                    <Card 
+                        key={questions[4].questionId} 
+                        question={questions[4].question}
+                        correctAnswerMarkUpdate={this.updateScore}
+                        attempt={this.nextQuest}
+                        options={{
+                            option1: questions[4].option1,
+                            option2: questions[4].option2,
+                            option3: questions[4].option3,
+                            option4: questions[4].option4
+                        }}
+                        answer={questions[4].answer}
+                    />
+                </div>
+            </div>
+        </div>
+      )}
+      {this.state.showResult&&(
+        <div>
+          <Button onClick={this.ResultHandler} disabled={false}>Show Results</Button>
+        </div>
+      )}
+      </div>
+  );}
+}
+
+export default App;
--- reactapp/src/components/UI/Button/Button.js
+++ reactapp/src/components/UI/Button/Button.js
@@ -1,6 +1,6 @@
 const Button = (props) => {
     const { btnName = props.children, onClick, marked, style } = props;
     return (
-        <button onClick={onClick} disabled={marked} className={style} >{btnName}</button>
+        <button onClick={onClick} disabled={marked} className={style} >{btnName}</button>
     )
 }
