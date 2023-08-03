cbbc28f30c4c60e0cd51a616f155cfc18a772731
Iamneo <techadmin@iamneo.ai>
2023-08-03 14:39:05 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,100 +1,129 @@
-import styles from "./App.module.css";
-import Banner from "./components/UI/Banner/Banner";
-import { useState } from "react";
-import Button from "./components/UI/Button/Button";
-import QuizData from "./Data/Data";
-import Card from "./components/UI/Card/Card";
-
-const resultBanner = {
-  backgroundColor: "#FA5F55",
-  borderRadius: "2%",
-  fontWeight: "bold",
-};
-
-const startButtonStyle = {
-  backgroundColor: "#FFDBAC",
-  textAlign: "center",
-  width: "100%",
-};
-
-const optionsButtonStyle = {
-  backgroundColor: "#FFDBAC",
-  textAlign: "center",
-  width: "100%",
-};
-
-const showResultButtonStyle = {
-  backgroundColor: "green",
-  textAlign: "center",
-  color: "white",
-};
+import { useState } from 'react';
+import './App.module.css';
+import Banner from './components/UI/Banner/Banner';
+import Button from './components/UI/Button/Button';
+import Card from './components/UI/Card/Card';
 
 function App() {
-  const [showResult, setShowResult] = useState(false);
-  const [showStartBtn, setShowStartBtn] = useState(true);
-  const [countCorrectAnswers, setCountCorrectAnswers] = useState(0);
-
-  function onClickHandler() {
-    setShowStartBtn(!showStartBtn);
-    setShowResult(false);
-    setCountCorrectAnswers(0);
+  const correctObject = {
+    questionsCorrect : 0
   }
 
-  function showResultHandler() {
-    setShowStartBtn(!showStartBtn);
-    setShowResult(false);
-    setShowResult(true);
+  const [quizStartState,setQuizStartState] = useState(false);
+  let [questionsAnswered, setQuestionsAnswered] = useState(0);
+  let [correctAnswers, setCorrectAnswers] = useState(correctObject);
+  const [showResults, setShowResults] = useState(false);
+
+  const questions = [
+    {
+      questionId : 1,
+      question : 'Who is the father of our nation?',
+      option1 : 'Mahatma Gandhi',
+      option2 : 'Jawaharlal Nehru',
+      option3 : 'Donald Trump',
+      option4 : 'Barrack Obama',
+      answer : 'Mahatma Gandhi',
+      isAnswered:false
+    },
+    {
+      questionId : 2,
+      question : 'What color is are the leaves ?',
+      option1 : 'Blue',
+      option2 : 'Red',
+      option3 : 'Yellow',
+      option4 : 'Green',
+      answer : 'Green',
+      isAnswered:false
+    },
+    {
+      questionId : 3,
+      question : 'What color is the sky ?',
+      option1 : 'Blue',
+      option2 : 'Red',
+      option3 : 'Yellow',
+      option4 : 'Green',
+      answer : 'Blue',
+      isAnswered:false
+    },
+    {
+      questionId : 4,
+      question : 'What color is the sky ?',
+      option1 : 'Blue',
+      option2 : 'Red',
+      option3 : 'Yellow',
+      option4 : 'Green',
+      answer : 'Blue',
+      isAnswered:false
+    },
+    {
+      questionId : 5,
+      question : 'What color is the fire ?',
+      option1 : 'Blue',
+      option2 : 'Red',
+      option3 : 'Yellow',
+      option4 : 'Green',
+      answer : 'Yellow',
+      isAnswered:false
+    }
+  ];
+
+  const [questionsState, setQuestionsState] = useState(questions);
+
+  const quizStartHandler = () => {
+    setQuizStartState(true);
+    setShowResults(false);
+    setQuestionsState(questions);
+    setCorrectAnswers(correctObject);
+  };
+
+  const attemptHandler = (event) => {
+    setQuestionsAnswered(++questionsAnswered);
+
+    const nextQuestionArray = [...questionsState];
+    const questionToBeChanged = nextQuestionArray.find(a => 
+      // eslint-disable-next-line
+      a.questionId == event.target.parentNode.id
+    );
+    
+    questionToBeChanged.isAnswered=true;
+    setQuestionsState(nextQuestionArray);
+
+    if(questionToBeChanged.answer === event.target.value) {
+      setCorrectAnswers({
+        questionsCorrect:++correctAnswers.questionsCorrect
+      });
+    }
   }
+
+  const resultsShowHandler = () => {
+    setShowResults(true);
+    setQuizStartState(false);
+    setQuestionsAnswered(correctObject);
+  };
+
+  const questionsCards = questionsState.map((question) => {
+    return <Card key={question.questionId}
+    id={question.questionId}
+    question={question.question}
+    attempt={attemptHandler}
+    options={{
+    option1: question.option1,
+    option2: question.option2,
+    option3: question.option3,
+    option4: question.option4
+    }}
+    isDisabled={question.isAnswered}
+    />;
+  }); 
 
   return (
-    <div className={styles.Wrapper}>
+    <div className="App">
       <h1>Quizz App</h1>
-
-      <ResultBanner showResult={showResult} result={countCorrectAnswers} />
-
-      <div className={styles.StartButton} onClick={onClickHandler}>
-        {showStartBtn && (
-          <Button buttonText={"Start Quiz"} ButtonStyle={startButtonStyle} />
-        )}
-      </div>
-
-      {!showStartBtn && (
-        <div className={styles.QuizDataContainer}>
-          {QuizData.map((value, index) => {
-            return (
-              <Card
-                key={index}
-                value={value}
-                ButtonStyle={optionsButtonStyle}
-                countCorrectAnswers={countCorrectAnswers}
-                setCountCorrectAnswers={setCountCorrectAnswers}
-              />
-            );
-          })}
-        </div>
-      )}
-      {!showStartBtn && (
-        <div className={styles.ShowResultButton} onClick={showResultHandler}>
-          <Button
-            buttonText={"Show Result"}
-            ButtonStyle={showResultButtonStyle}
-          />
-        </div>
-      )}
+      {(showResults) ? <Banner correct={correctAnswers.questionsCorrect}/>:''}
+      {(!quizStartState) ? <Button handler={quizStartHandler}>Start Quiz</Button>:''}
+      {(quizStartState) ? <div>{questionsCards}</div> : ''}
+      {(questionsAnswered === 5) ? <Button handler={resultsShowHandler}>Show Results</Button>:''}
     </div>
-  );
-}
-
-function ResultBanner({ showResult, result }) {
-  return (
-    <>
-      {showResult && (
-        <Banner
-          styleObject={resultBanner}
-          text={`You have answered ${result}/5 correctly`}
-        />
-      )}
-    </>
   );
 }
 
