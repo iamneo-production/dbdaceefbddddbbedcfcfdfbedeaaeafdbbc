52959bb533c5c6bfffebbe4c04f7fb482842f4d1
Iamneo <techadmin@iamneo.ai>
2023-08-03 14:29:05 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,161 +1,101 @@
-import React, { useState } from 'react';
-import './App.css';
-import Banner from './components/UI/Banner/Banner';
-import Button from './components/UI/Button/Button';
-import Card from './components/UI/Card/Card';
-import questions from './Data/Data';
+import styles from "./App.module.css";
+import Banner from "./components/UI/Banner/Banner";
+import { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import QuizData from "./Data/Data";
+import Card from "./components/UI/Card/Card";
 
-class App extends React.Component{
+const resultBanner = {
+  backgroundColor: "#FA5F55",
+  borderRadius: "2%",
+  fontWeight: "bold",
+};
 
-  constructor(){
-    super();
-    this.state={
-      showStart: true,
-      showResultBanner: false,
-      questionsCorrect: 0,
-      currQuestion: 0,
-      showResult: false
-    }
+const startButtonStyle = {
+  backgroundColor: "#FFDBAC",
+  textAlign: "center",
+  width: "100%",
+};
+
+const optionsButtonStyle = {
+  backgroundColor: "#FFDBAC",
+  textAlign: "center",
+  width: "100%",
+};
+
+const showResultButtonStyle = {
+  backgroundColor: "green",
+  textAlign: "center",
+  color: "white",
+};
+
+function App() {
+  const [showResult, setShowResult] = useState(false);
+  const [showStartBtn, setShowStartBtn] = useState(true);
+  const [countCorrectAnswers, setCountCorrectAnswers] = useState(0);
+
+  function onClickHandler() {
+    setShowStartBtn(!showStartBtn);
+    setShowResult(false);
+    setCountCorrectAnswers(0);
   }
 
-  startHandler = () => {
-    this.setState({
-      showStart:false,
-      showResult:false,
-      showResultBanner:false,
-      questionsCorrect:0,
-      currQuestion:0
-    })
+  function showResultHandler() {
+    setShowStartBtn(!showStartBtn);
+    setShowResult(false);
+    setShowResult(true);
   }
 
-  ResultHandler = () => {
-    this.setState({
-      showResultBanner:true,
-      showStart:true,
-      showResult:false
-    })
-  }
-
-  updateScore=()=>{
-    if(this.state.questionsCorrect<5){
-      this.setState({
-        questionsCorrect:this.state.questionsCorrect+1
-      })
-    }
-  }
-
-  nextQuest=()=>{
-    if(this.state.currQuestion==4){
-      this.setState({
-        showResult:true
-      })
-    }
-    this.setState({
-      currQuestion: this.state.currQuestion+1
-    })
-  }
-  render(){
-    return (
-    <div>
+  return (
+    <div className={styles.Wrapper}>
       <h1>Quizz App</h1>
-      {this.state.showResultBanner&&(
-        <Banner score={this.state.questionsCorrect} />
-      )}
-      {this.state.showStart&&(
-        <div>
-          <Button onClick={this.startHandler} disabled={false} >Start Quiz</Button>
-        </div>
-      )}
-      {!(this.state.showStart)&&(
-        <div>
-          <div className="parent">
-                <div className="child">
-                    <Card 
-                        key={questions[0].questionId} 
-                        question={questions[0].question}
-                        correctAnswerMarkUpdate={this.updateScore}
-                        attempt={this.nextQuest}
-                        options={{
-                            option1: questions[0].option1,
-                            option2: questions[0].option2,
-                            option3: questions[0].option3,
-                            option4: questions[0].option4
-                        }}
-                        answer={questions[0].answer}
-                    />
-                </div>
-                <div className="child">
-                    <Card 
-                        key={questions[1].questionId} 
-                        question={questions[1].question}
-                        correctAnswerMarkUpdate={this.updateScore}
-                        attempt={this.nextQuest}
-                        options={{
-                            option1: questions[1].option1,
-                            option2: questions[1].option2,
-                            option3: questions[1].option3,
-                            option4: questions[1].option4
-                        }}
-                        answer={questions[1].answer}
-                    />
-                </div>
-            </div>
-            <div>
-                <div className="child">
-                    <Card 
-                        key={questions[2].questionId} 
-                        question={questions[2].question}
-                        correctAnswerMarkUpdate={this.updateScore}
-                        attempt={this.nextQuest}
-                        options={{
-                            option1: questions[2].option1,
-                            option2: questions[2].option2,
-                            option3: questions[2].option3,
-                            option4: questions[2].option4
-                        }}
-                        answer={questions[2].answer}
-                    />
-                </div>
-                <div className="child">
-                    <Card 
-                        key={questions[3].questionId} 
-                        question={questions[3].question}
-                        correctAnswerMarkUpdate={this.updateScore}
-                        attempt={this.nextQuest}
-                        options={{
-                            option1: questions[3].option1,
-                            option2: questions[3].option2,
-                            option3: questions[3].option3,
-                            option4: questions[3].option4
-                        }}
-                        answer={questions[3].answer}
-                    />
-                </div>
-                <div className="child">
-                    <Card 
-                        key={questions[4].questionId} 
-                        question={questions[4].question}
-                        correctAnswerMarkUpdate={this.updateScore}
-                        attempt={this.nextQuest}
-                        options={{
-                            option1: questions[4].option1,
-                            option2: questions[4].option2,
-                            option3: questions[4].option3,
-                            option4: questions[4].option4
-                        }}
-                        answer={questions[4].answer}
-                    />
-                </div>
-            </div>
-        </div>
-      )}
-      {this.state.showResult&&(
-        <div>
-          <Button onClick={this.ResultHandler} disabled={false}>Show Results</Button>
-        </div>
-      )}
+
+      <ResultBanner showResult={showResult} result={countCorrectAnswers} />
+
+      <div className={styles.StartButton} onClick={onClickHandler}>
+        {showStartBtn && (
+          <Button buttonText={"Start Quiz"} ButtonStyle={startButtonStyle} />
+        )}
       </div>
-  );}
+
+      {!showStartBtn && (
+        <div className={styles.QuizDataContainer}>
+          {QuizData.map((value, index) => {
+            return (
+              <Card
+                key={index}
+                value={value}
+                ButtonStyle={optionsButtonStyle}
+                countCorrectAnswers={countCorrectAnswers}
+                setCountCorrectAnswers={setCountCorrectAnswers}
+              />
+            );
+          })}
+        </div>
+      )}
+      {!showStartBtn && (
+        <div className={styles.ShowResultButton} onClick={showResultHandler}>
+          <Button
+            buttonText={"Show Result"}
+            ButtonStyle={showResultButtonStyle}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function ResultBanner({ showResult, result }) {
+  return (
+    <>
+      {showResult && (
+        <Banner
+          styleObject={resultBanner}
+          text={`You have answered ${result}/5 correctly`}
+        />
+      )}
+    </>
+  );
 }
 
 export default App;
